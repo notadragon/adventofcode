1395c8256864f488f42eb8d4d5c6ceb2dc6f2d34
Joshua Berne <jberne4@bloomberg.net>
2019-12-05 16:22:52 +0000

    day 5 done

--- 2019/5/1.py
+++ 2019/5/1.py
@@ -17,7 +17,7 @@ if not args.p1 and not args.p2:
 
 print("Input: %s P1: %s p2: %s" % (args.input,args.p1,args.p2))
 
-lineRe = re.compile(".*")
+lineRe = re.compile("(-?\d+)(?:,(-?\d+))*.*")
 
 for x in open(args.input).readlines():
     x = x.strip()
@@ -29,11 +29,100 @@ for x in open(args.input).readlines():
         print("Invalid line: %s" % (x,))
         
     # Process input line
+    values = [ int(n) for n in x.split(",") ]
 
+print("Vals: %s" % (values,))
+
+def params(pmode, paramtypes, pos, values):
+    #print("pmode: %s paramtypes: %s pos: %s values: %s" % (pmode, paramtypes, pos, values,))
+    output = []
+    i = 0
+    while i < len(paramtypes):
+        p = values[pos + i]
+        imode = pmode % 10
+        itype = paramtypes[i]
+        #print("i: %s pmode: %s itype: %s imode: %s p: %s" % (i,pmode,itype,imode,p,))
+        if itype == 1:
+            if imode == 0:
+                p = values[p]
+        output.append(p)
+        i = i + 1
+        pmode /= 10
+    #print("output: %s" % (output,))
+    return tuple(output)
+
+def runprogram1(values, input):
+    output = []
+    pos = 0
+    vals = list(values)
+    while True:
+        if pos >= len(vals):
+            return (vals,output)
+        instruction = vals[pos]
+        opcode = instruction % 100
+        pmode = instruction / 100
+        if opcode == 99:
+            return (vals,output)
+        elif opcode == 1:
+            i1,i2,i3 = params(pmode, (1,1,0), pos+1, vals)
+            vals[i3] = i1 + i2
+            pos = pos + 4
+        elif opcode == 2:
+            i1,i2,i3 = params(pmode, (1,1,0), pos+1, vals)
+            vals[i3] = i1 * i2
+            pos = pos + 4
+        elif opcode == 3:
+            (i1,) = params(pmode, (0,), pos+1, vals)
+            vals[i1] = input.next()
+            pos = pos + 2
+        elif opcode == 4:
+            (i1,) = params(pmode, (1,), pos+1, vals)
+            output.append(i1)
+            pos = pos + 2
+        elif opcode == 5:
+            (i1,i2) = params(pmode, (1,1,), pos+1, vals)
+            if i1 != 0:
+                pos = i2
+            else:
+                pos = pos + 3
+        elif opcode == 6:
+            (i1,i2) = params(pmode, (1,1,), pos+1, vals)
+            if i1 == 0:
+                pos = i2
+            else:
+                pos = pos + 3
+        elif opcode == 7:
+            (i1,i2,i3) = params(pmode, (1,1,0), pos+1, vals)
+            if i1 < i2:
+                vals[i3] = 1
+            else:
+                vals[i3] = 0
+            pos = pos + 4
+        elif opcode == 8:
+            (i1,i2,i3) = params(pmode, (1,1,0), pos+1, vals)
+            if i1 == i2:
+                vals[i3] = 1
+            else:
+                vals[i3] = 0
+            pos = pos + 4
+        else:
+            print("Unknown opcode: %s" % (opcode,))
+            return (vals,output)
+        
 
 if args.p1:
     print("Doing part 1")
 
+    vals,output = runprogram1(values, iter([1]))
+
+    print("Vals: %s" % (vals,))
+    print("Output: %s" % (output,))
     
 if args.p2:
     print("Doing part 2")
+
+    vals,output = runprogram1(values, iter([5]))
+
+    print("Vals: %s" % (vals,))
+    print("Output: %s" % (output,))
+    
