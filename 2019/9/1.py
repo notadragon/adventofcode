e92ae6fe0da8ba63261e6664959138e4c0daeb79
Joshua Berne <jberne4@bloomberg.net>
2019-12-09 14:59:56 +0000

    day 9 complete

--- 2019/9/1.py
+++ 2019/9/1.py
@@ -33,70 +33,113 @@ for x in open(args.input).readlines():
 
 #print("Vals: %s" % (vals,))
 
+def params(pmode, paramtypes, pos, relbase, values):
+    #print("pmode: %s paramtypes: %s pos: %s values: %s" % (pmode, paramtypes, pos, values,))
+    output = []
+    i = 0
+    while i < len(paramtypes):
+        p = values.get(pos + 1 + i,0)
+        imode = pmode % 10
+        itype = paramtypes[i]
+        #print("i: %s pmode: %s itype: %s imode: %s p: %s" % (i,pmode,itype,imode,p,))
+        if itype == 1:
+            if imode == 0:
+                p = values.get(p,0)
+            elif imode == 1:
+                p = p
+            elif imode == 2:
+                p = values.get(p + relbase,0)
+        elif itype == 0:
+            if imode == 0:
+                p = p
+            elif imode == 2:
+                p = p + relbase
+        output.append(p)
+        i = i + 1
+        pmode /= 10
+    #print("output: %s" % (output,))
+    return tuple(output)
+
 def genoutput(values, input):
     ipos = 0
     pos = 0
-    vals = list(values)
+    relbase = 0
+    vals = { i:v for i,v in enumerate(values) }
     while True:
-        if pos >= len(vals):
-            return
-        instruction = vals[pos]
+        #print("pos: %s" % (pos,))
+        #print("Vals: %s" % (vals,))
+        instruction = vals.get(pos,0)
         opcode = instruction % 100
         pmode = instruction / 100
         if opcode == 99:
             return
         elif opcode == 1:
-            i1,i2,i3 = params(pmode, (1,1,0), pos+1, vals)
+            i1,i2,i3 = params(pmode, (1,1,0), pos, relbase, vals)
             vals[i3] = i1 + i2
             pos = pos + 4
         elif opcode == 2:
-            i1,i2,i3 = params(pmode, (1,1,0), pos+1, vals)
+            i1,i2,i3 = params(pmode, (1,1,0), pos, relbase, vals)
             vals[i3] = i1 * i2
             pos = pos + 4
         elif opcode == 3:
-            (i1,) = params(pmode, (0,), pos+1, vals)
+            (i1,) = params(pmode, (0,), pos, relbase, vals)
             if ipos >= len(input):
                 yield None
             vals[i1] = input[ipos]
             ipos = ipos + 1
             pos = pos + 2
         elif opcode == 4:
-            (i1,) = params(pmode, (1,), pos+1, vals)
+            (i1,) = params(pmode, (1,), pos, relbase, vals)
             yield i1
             pos = pos + 2
         elif opcode == 5:
-            (i1,i2) = params(pmode, (1,1,), pos+1, vals)
+            (i1,i2) = params(pmode, (1,1,), pos, relbase, vals)
             if i1 != 0:
                 pos = i2
             else:
                 pos = pos + 3
         elif opcode == 6:
-            (i1,i2) = params(pmode, (1,1,), pos+1, vals)
+            (i1,i2) = params(pmode, (1,1,), pos, relbase, vals)
+            #print("i1,i2: %s" % ( (i1,i2,), ) )
             if i1 == 0:
                 pos = i2
             else:
                 pos = pos + 3
         elif opcode == 7:
-            (i1,i2,i3) = params(pmode, (1,1,0), pos+1, vals)
+            (i1,i2,i3) = params(pmode, (1,1,0), pos, relbase, vals)
             if i1 < i2:
                 vals[i3] = 1
             else:
                 vals[i3] = 0
             pos = pos + 4
         elif opcode == 8:
-            (i1,i2,i3) = params(pmode, (1,1,0), pos+1, vals)
+            (i1,i2,i3) = params(pmode, (1,1,0), pos, relbase, vals)
             if i1 == i2:
                 vals[i3] = 1
             else:
                 vals[i3] = 0
             pos = pos + 4
+        elif opcode == 9:
+            (i1,) = params(pmode, (1,), pos, relbase, vals)
+            relbase = relbase + i1
+            pos = pos + 2
         else:
             print("Unknown opcode: %s" % (opcode,))
             return
 
+
+        
 if args.p1:
     print("Doing part 1")
 
+    g = genoutput(vals, [1])
+    for i in g:
+        print("i:%s" % (i,))
     
 if args.p2:
     print("Doing part 2")
+
+    g = genoutput(vals, [2])
+    for i in g:
+        print("i:%s" % (i,))
+    
