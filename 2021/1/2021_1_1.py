41faee89bd12e832cf3bfc9224d0ebb1267632c1
notadragon <berne@notadragon.com>
2021-12-01 20:45:00 -0500

    day 1 done

---
+++ 2021/1/2021_1_1.py
@@ -0,0 +1,60 @@
+#!/usr/bin/env pypy
+
+import argparse, re, itertools, collections
+
+parser = argparse.ArgumentParser()
+parser.add_argument("input",type=str,nargs='?',default="input")
+parser.add_argument("--p1",dest="p1",action='store_true')
+parser.add_argument("--no-p1",dest="p1",action='store_false')
+parser.add_argument("--p2",dest="p2",action='store_true')
+parser.add_argument("--no-p2",dest="p2",action='store_false')
+
+args = parser.parse_args()
+
+if not args.p1 and not args.p2:
+    args.p1 = True
+    args.p2 = True
+
+print("Input: %s P1: %s p2: %s" % (args.input,args.p1,args.p2))
+
+lineRe = re.compile("\d+")
+
+data = []
+
+for x in open(args.input).readlines():
+    x = x.strip()
+    if not x:
+        continue
+
+    m = lineRe.match(x)
+    if not m:
+        print("Invalid line: %s" % (x,))
+        
+    # Process input line
+    data.append(int(x))
+
+print("Data: %s" % (data,))
+
+if args.p1:
+    print("Doing part 1")
+
+    incr = 0
+    for x,y in zip(data[:-1],data[1:]):
+        if y > x:
+            incr = incr + 1
+
+    print("Increases: %s" % (incr,))
+    
+    
+if args.p2:
+    print("Doing part 2")
+
+    sums = [ x + y + z for x,y,z in zip(data[:-2],data[1:-1],data[2:]) ]
+    
+
+    incr = 0
+    for x,y in zip(sums[:-1],sums[1:]):
+        if y > x:
+            incr = incr + 1
+
+    print("Increases: %s" % (incr,))
